0e04f097cbadc3e20ec27e365602da02d81afd3a
Graham Hall <graham@ghall.dev>
2025-01-17 08:49:41 -0500

    add autodock

---
+++ Casks/autodock.rb
@@ -0,0 +1,19 @@
+cask "autodock" do
+  version "1.0.0"
+  sha256 "a319c0cf125f36063922647828fc1c89f80ada9c996f8266a671e2a0fb2a9e71"
+
+  url "https://github.com/ghall89/AutoDock/releases/download/v#{version}/AutoDock.zip"
+
+  name "AutoDock"
+  desc "Software license key manager"
+  homepage "https://github.com/ghall89/AutoDock"
+
+  depends_on macos: ">= :sequoia"
+
+  app "KeyStash.app"
+
+  zap trash: [
+  	"~/Library/Preferences/com.ghall.dev.AutoDock.plist",
+  	"~/Library/Saved Application State/com.ghall.dev.AutoDock.savedState"
+  ]
+end
