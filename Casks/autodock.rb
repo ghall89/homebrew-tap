181e5276b43b0d80def127a7e7e4909aa1229595
Graham Hall <graham@ghall.dev>
2025-09-19 11:03:56 -0400

    update autodock

--- Casks/autodock.rb
+++ Casks/autodock.rb
@@ -1,19 +1,19 @@
-cask "autodock" do
-  version "1.2.0"
-  sha256 "442dadfd97f78645949d9f179cced19b94c74c2cf04bfea53a46396e39d2d7e8"
+cask 'autodock' do
+  version '1.2.1'
+  sha256 '447f63f41bc64656befaa4d9bbefa2ee0993daf08d36a247f89655bfc4ac1f12'
 
   url "https://github.com/ghall89/AutoDock/releases/download/v#{version}/AutoDock.zip"
 
-  name "AutoDock"
-  desc "Utility for automatically hiding and showing the MacOS Dock"
-  homepage "https://github.com/ghall89/AutoDock"
+  name 'AutoDock'
+  desc 'Utility for automatically hiding and showing the MacOS Dock'
+  homepage 'https://github.com/ghall89/AutoDock'
 
-  depends_on macos: ">= :sequoia"
+  depends_on macos: '>= :sequoia'
 
-  app "AutoDock.app"
+  app 'AutoDock.app'
 
   zap trash: [
-  	"~/Library/Preferences/com.ghall.dev.AutoDock.plist",
-  	"~/Library/Saved Application State/com.ghall.dev.AutoDock.savedState"
+    '~/Library/Preferences/com.ghall.dev.AutoDock.plist',
+    '~/Library/Saved Application State/com.ghall.dev.AutoDock.savedState'
   ]
 end
