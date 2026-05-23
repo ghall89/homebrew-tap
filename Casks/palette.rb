3a2a45ff1322c34cb811f9c16c901f89a03e18c3
Graham Hall <graham@ghall.dev>
2026-05-23 19:07:01 -0400

    fix typo

--- Casks/palette.rb
+++ Casks/palette.rb
@@ -2,7 +2,7 @@ cask 'palette' do
   version '0.1.0-10'
   sha256 '8f792e9ab7485ad554a9a34d59040acc398862a1660f46d0cefb1c4e0fa7a020'
 
-  url "https://cdn.amore.computer/releases/com.ghalldev.Palette/#[version}/Palette.zip"
+  url "https://cdn.amore.computer/releases/com.ghalldev.Palette/#{version}/Palette.zip"
 
   name 'Palette'
   desc 'A universal command palette for your Mac'
