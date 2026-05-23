f9e51a61d352d4f0dd97c364a1f031cac24991da
Graham Hall <graham@ghall.dev>
2026-05-23 19:02:26 -0400

    add casks/palette

---
+++ Casks/palette.rb
@@ -0,0 +1,18 @@
+cask 'palette' do
+  version '0.1.0-10'
+  sha256 '8f792e9ab7485ad554a9a34d59040acc398862a1660f46d0cefb1c4e0fa7a020'
+
+  url "https://cdn.amore.computer/releases/com.ghalldev.Palette/#[version}/Palette.zip"
+
+  name 'Palette'
+  desc 'A universal command palette for your Mac'
+  homepage 'https://codeberg.org/ghalldev/Palette'
+
+  depends_on macos: '>= :tahoe'
+
+  app 'Palette.app'
+
+  zap trash: [
+    '~/Library/Preferences/com.ghalldev.Palette.plist'
+  ]
+end
