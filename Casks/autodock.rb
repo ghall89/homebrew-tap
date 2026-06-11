d570e9252520062ce86a337f2b3e13867f0f85f2
Graham Hall <graham@ghall.dev>
2026-06-11 14:10:00 -0400

    fix autodock definition

--- Casks/autodock.rb
+++ Casks/autodock.rb
@@ -2,7 +2,7 @@ cask 'autodock' do
   version '1.3.1'
   sha256 '07a31f7309a6111891d9099f2c0dd10a2a1ae107a0f84cf211fb107bc358ae67'
 
-  url 'https://github.com/ghall89/AutoDock/releases/download/v#{version}/AutoDock.dmg'
+  url "https://github.com/ghall89/AutoDock/releases/download/v#{version}/AutoDock.dmg"
 
   name 'AutoDock'
   desc 'Utility for automatically hiding and showing the macOS Dock'
